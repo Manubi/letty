a91cb25e42930aeee18c025a8962e22793cd58b0
Manuel Bichler <bichler@gmail.com>
2022-11-05 18:30:05 +0100

    added actix web, tokio and serde

---
+++ scripts/init_db.sh
@@ -0,0 +1,10 @@
+DB_PORT="${POSTGRES_PORT:=5432}"
+# Launch postgres using Docker
+docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000
+  # ^ Increased maximum number of connections for testing purposes
